b22c2bb51c22cff2a4cde1d13d4962b1fe755a00
Pandey, Ravi <RaviPandey.tech@gmail.com>
2026-09-04 17:34:13 +0530

    added getUserById metod in UserService

--- src/main/java/com/ravi/lms/service/UserService.java
+++ src/main/java/com/ravi/lms/service/UserService.java
@@ -5,6 +5,8 @@ import com.ravi.lms.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -14,7 +16,7 @@ public class UserService {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
-    
+
 
     public User registerUser(User user) {
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
@@ -22,5 +24,10 @@ public class UserService {
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found wit Id:" + id);
+
     }
 }
